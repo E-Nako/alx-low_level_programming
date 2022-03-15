f50a6b98dfa21ad8f52e40a37e43d2b97272bd46
E-Nako <enakotey44@gmail.com>
2022-03-15 12:52:07 -0700

    2. 10 x alphabet updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,19 @@
-#include "mai.h"
-
+#include "main.h"
 /**
- * main - check the code for ALX School students.
+ * print_alphabet_x10 - function to print abc 10 times
  *
- * Return: Always 0.
+ * Return: 0
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-    print_alphabet_x10();
-    return (0);
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
 }
